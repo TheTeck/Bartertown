31925a9e87318a0189bd5158abcc6c200e66298c
John Teckemeyer <johnteckemeyer@gmail.com>
2021-03-16 12:22:04 -0700

    Style site with Materialize

--- controllers/index.js
+++ controllers/index.js
@@ -10,7 +10,7 @@ async function index (req, res, next) {
   try {
     const proposals = await Proposal.find({})
     const bids = await Bid.find({})
-    console.log(bids)
+    if (bids.length > 20) bids.slice(0, 20)
     res.render('index', { name: 'Stranger', proposals })
   } catch (err) {
     res.send(err)
