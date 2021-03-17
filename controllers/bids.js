a0ae777a2550ef894df5459fbc7379a9d6c939c6
John Teckemeyer <johnteckemeyer@gmail.com>
2021-03-16 21:57:37 -0700

    Further styling with Materialize

--- controllers/bids.js
+++ controllers/bids.js
@@ -1,5 +1,4 @@
 const Proposal = require('../models/proposal')
-const User = require('../models/user')
 const Bid = require('../models/bid')
 const fs = require('fs')
 const path = require('path')
@@ -46,8 +45,9 @@ async function show (req, res) {
     try {
         // Get the bid to show in view
         const bid = await Bid.findById(req.params.id)
-        console.log('showing bid')
-        res.render('bids/show', { name: req.user.username, bid , isOwner: req.user._id.equals(bid.owner) })
+        const proposal = await Proposal.findById(bid.parentProposal)
+        res.render('bids/show', { name: req.user.username, bid , isPropOwner: proposal.owner.equals(req.user.id),
+                                 isOwner: req.user._id.equals(bid.owner) })
     } catch (err) {
         res.send(err)  
     }
